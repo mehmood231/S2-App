38dff2b2691f91381f0674b7b0f4f907eba1d06c
User <mahmoodahmad786786gmail.com>
2020-12-05 15:01:10 +0500

    Navigation Drawing Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-import 'package:contactus/contactus.dart';
 import 'package:flutter/material.dart';
 void main() {
+  import 'package:flutter/material.dart';
+
+  void main() => runApp(MyApp());
+
   class MyApp extends StatelessWidget {
+  final appTitle = 'Drawer Demo';
+
   @override
   Widget build(BuildContext context) {
   return MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: Scaffold(
-  backgroundColor: Colors.teal,
-  body: ContactUs(
-  logo: AssetImage('images/crop.jpg'),
-  email: 'adoshi26.ad@gmail.com',
-  companyName: 'Abhishek Doshi',
-  phoneNumber: '+91123456789',
-  website: 'https://abhishekdoshi.godaddysites.com',
-  githubUserName: 'AbhishekDoshi26',
-  linkedinURL: 'https://www.linkedin.com/in/abhishek-doshi-520983199/',
-  tagLine: 'Flutter Developer',
-  twitterHandle: 'AbhishekDoshi26',
-  instagramUserName: '_abhishek_doshi',
+  title: appTitle,
+  home: MyHomePage(title: appTitle),
+  );
+  }
+  }
+  class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(title: Text(title)),
+  body: Center(child: Text('My Page!')),
+  drawer: Drawer(
+
+  child: ListView(
+  padding: EdgeInsets.zero,
+  children: <Widget>[
+  DrawerHeader(
+  child: Text('Drawer Header'),
+  decoration: BoxDecoration(
+  color: Colors.blue,
+  ),
+  ),
+  ListTile(
+  title: Text('Item 1'),
+  onTap: () {
+
+  Navigator.pop(context);
+  },
+  ),
+  ListTile( title: Text('Item 2'),
+  onTap: () {
+
+  Navigator.pop(context);
+  },
+  ),
+  ],
   ),
   ),
   );
